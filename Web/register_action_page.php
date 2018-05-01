81c8d575c50a3cfda206ef29da61c0d057598cf8
Trey Ridley <treyridley@Treys-MacBook.local>
2018-05-01 03:19:33 -0400

    insert working

--- Web/register_action_page.php
+++ Web/register_action_page.php
@@ -1,6 +1,6 @@
 <?php
 //placeholders for variables
-//import (../helpers.php);
+//import (./helpers.php);
 
 
 $username = $_POST["username"];
@@ -13,18 +13,17 @@ $dob = $_POST["dob"];
 $acc_val = $_POST["account_value"];
 
 
-$dbc = mysqli_connect('localhost', 'user_web', 'jawnskie2', 'investment_club_dbs')
-    or die('Could not connect: ' . mysqli_error());
+$dbc = mysqli_connect('localhost', 'trey', 'poop', 'investment_club_dbs')
+    or die('Could not connect: ' . mysqli_connect_error());
 	
-$query = "INSERT INTO user (username, name_first, name_last, password_str, email_address, phone, dob, account_value)
+$query = "INSERT INTO users (username, name_first, name_last, password_str, email_address, phone, dob, account_value)
 									VALUES ('$username', '$f_name', '$l_name', '$passwrd', '$email', '$phone', '$dob', '$acc_val')";
 									
-//$response = @mysqli_query($dbc, $query);
 
 if (mysqli_query($dbc, $query)) {
     echo "New record created successfully";
 } else {
-    echo "Error: ";
+    echo "Error: " . $query . "<br>" . mysqli_error($dbc);
 }
 
 mysqli_close($dbc);
--- mysqli_connect.php
+++ mysqli_connect.php
@@ -1,8 +1,8 @@
 <?php
 
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'user_web');
-define('DB_PASSWORD', 'jawnskie2');
+define('DB_USERNAME', 'trey');
+define('DB_PASSWORD', 'poop');
 define('DB_NAME', 'investment_club_dbs');
 
 $dbc = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME)
